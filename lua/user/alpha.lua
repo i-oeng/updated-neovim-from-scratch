93299c9b5f739385d4c1666fabd34eb63532f34f
i-eong <islam.kossymbayev@nu.edu.kz>
2026-06-29 17:29:28 +0500

    Adjust spacing and change the ASCII art

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -4,15 +4,33 @@ if not status_ok then
 end
 
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
-}
-dashboard.section.buttons.val = {
+local full_header = vim.split([[
+⣿⣿⣿⣿⣿⣷⣿⣿⣿⡅⡹⢿⠆⠙⠋⠉⠻⠿⣿⣿⣿⣿⣿⣿⣮⠻⣦⡙⢷⡑⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣌⠡⠌⠂⣙⠻⣛⠻⠷⠐⠈⠛⢱⣮⣷⣽⣿
+⣿⣿⣿⣿⡇⢿⢹⣿⣶⠐⠁⠀⣀⣠⣤⠄⠀⠀⠈⠙⠻⣿⣿⣿⣦⣵⣌⠻⣷⢝⠦⠚⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢟⣻⣿⣊⡃⠀⣙⠿⣿⣿⣿⣎⢮⡀⢮⣽⣿⣿
+⢿⣿⣿⣿⣧⡸⡎⡛⡩⠖⠀⣴⣿⣿⣿⠀⠀⠀⠀⠸⠇⠀⠙⢿⣿⣿⣿⣷⣌⢷⣑⢷⣄⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣫⠶⠛⠉⠀⠁⠀⠈⠈⠀⠠⠜⠻⣿⣆⢿⣼⣿⣿⣿
+⢐⣿⣿⣿⣿⣧⢧⣧⢻⣦⢀⣹⣿⣿⣿⣇⠀⠄⠀⠀⠀⡀⠀⠈⢻⣿⣿⣿⣿⣷⣝⢦⡹⠷⡙⢿⣿⣿⣿⣿⣿⣿⣿⣿⠈⠁⠀⠀⠀⠁⠀⠀⠀⠱⣶⣄⡀⠀⠈⠛⠜⣿⣿⣿⣿
+⠀⠊⢫⣿⣏⣿⡌⣼⣄⢫⡌⣿⣿⣿⣿⣿⣦⡈⠲⣄⣤⣤⡡⢀⣠⣿⣿⣿⣿⣿⣿⣷⣼⣍⢬⣦⡙⣿⣿⣿⣿⣿⣯⢁⡄⠀⡀⡀⠀⠄⢈⣠⢪⠀⣿⣿⣿⣦⠀⢉⢂⠹⡿⣿⣿
+⠀⠀⠄⢹⢃⢻⣟⠙⣿⣦⠱⢻⣿⣿⣿⣿⣿⣿⣷⣬⣍⣭⣥⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⡙⢿⣼⡿⣿⣿⣿⣿⣿⣷⣄⠘⣱⢦⣤⡴⡿⢈⣼⣿⣿⣿⣇⣴⣶⣮⣅⢻⣿⡏
+⠀⠀⠈⠹⣇⢡⢿⡆⠻⣿⣷⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣍⡻⣿⣟⣻⣿⣿⣿⣿⣷⣦⣥⣬⣤⣴⣾⣿⣿⣿⣿⣷⣿⣿⣿⣿⣷⡜⠃
+⠀⠀⠀⢀⣘⠈⢂⠃⣧⡹⣿⣷⡄⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣮⣅⡙⢿⣟⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⡕⠂
+⠀⠀⠀⠀⠀⠀⠛⢷⣜⢷⡌⠻⣿⣿⣦⣝⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣹⣷⣦⣹⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠉⠃⠀⠀⠀
+]], "\n", { trimempty = true })
+
+dashboard.section.header.val = function()
+	local max_width = 0
+	for _, line in ipairs(full_header) do
+		max_width = math.max(max_width, vim.fn.strdisplaywidth(line))
+	end
+
+	local art_fits_width = max_width <= math.max(vim.o.columns - 4, 1)
+	local art_fits_height = (#full_header + 11) <= vim.o.lines
+	if art_fits_width and art_fits_height then
+		return full_header
+	end
+	return { "NVIM" }
+end
+
+local full_buttons = {
 	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
@@ -21,6 +39,27 @@ dashboard.section.buttons.val = {
 	dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
+
+local compact_buttons = {
+	dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "Projects", ":Telescope projects <CR>"),
+	dashboard.button("r", "Recent", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "Config", ":e $MYVIMRC <CR>"),
+	dashboard.button("q", "Quit", ":qa<CR>"),
+}
+
+dashboard.section.buttons.val = function()
+	local buttons = vim.o.columns < 42 and compact_buttons or full_buttons
+	local width = math.max(12, math.min(42, vim.o.columns - 4))
+	for _, button in ipairs(buttons) do
+		button.opts.width = width
+		button.opts.cursor = math.min(3, math.max(width - 2, 0))
+	end
+	return buttons
+end
+dashboard.section.buttons.opts.spacing = 1
 
 local function footer()
 -- NOTE: requires the fortune-mod package to work
@@ -31,12 +70,33 @@ local function footer()
 	return "Thou art a creator"
 end
 
-dashboard.section.footer.val = footer()
+dashboard.section.footer.val = function()
+	return vim.o.lines < 18 and "" or footer()
+end
 
 dashboard.section.footer.opts.hl = "Type"
 dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
+dashboard.opts.layout = {
+	{
+		type = "padding",
+		val = function()
+			return vim.o.lines < 22 and 0 or 1
+		end,
+	},
+	dashboard.section.header,
+	{
+		type = "padding",
+		val = function()
+			return vim.o.lines < 26 and 0 or 1
+		end,
+	},
+	dashboard.section.buttons,
+	dashboard.section.footer,
+}
+dashboard.opts.opts.margin = 1
+dashboard.opts.opts.redraw_on_resize = true
 dashboard.opts.opts.noautocmd = true
 -- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
