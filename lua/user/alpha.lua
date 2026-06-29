a8139c36599e0d90f9a27c58bac1b2ecef4798c0
i-eong <islam.kossymbayev@nu.edu.kz>
2026-06-29 16:58:12 +0500

    Updated whichkey.lua for newer Neovim versions

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "Thou art a creator"
 end
 
 dashboard.section.footer.val = footer()
